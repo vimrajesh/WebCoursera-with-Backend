116d0fd389b7b594070cfe6f5229f1a551095efa
Vimal Rajesh <vimrajesh@gmail.com>
2021-10-20 09:21:38 +0530

    Fixed Logout Unsetting Cookie

--- login/logout.php
+++ login/logout.php
@@ -1,6 +1,6 @@
 <?php
-    unset($_COOKIE['user']); 
-
+    unset($_COOKIE['user']);
+    setcookie('user', '', time() - 3600, '/');
     echo "<h3><center>"."Successfully Logged Out. "."</center></h3>".'<center><h3>'."Redirecting to HomePage.....".'</center></h3>';
     echo "<script>setTimeout(\"location.href = '../index.php';\",1500);</script>";
 ?>
