e1960e88330058b72a6f457c959c1171e8c5e0c6
Vimal Rajesh <vimrajesh@gmail.com>
2021-10-23 16:36:21 +0530

    fixed logout cookie for email

--- login/logout.php
+++ login/logout.php
@@ -1,6 +1,7 @@
 <?php
     unset($_COOKIE['user']);
     setcookie('user', '', time() - 3600, '/');
+    setcookie('email', '', time() - 3600, '/');
     echo "<h3><center>"."Successfully Logged Out. "."</center></h3>".'<center><h3>'."Redirecting to HomePage.....".'</center></h3>';
     echo "<script>setTimeout(\"location.href = '/webcoursera/index.php';\",1500);</script>";
 ?>
